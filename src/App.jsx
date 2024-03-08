1541c5cfa8930993a30442ee44a2804332c70b26
Md. Rezaul Islam Rana <rexaul.rana@gmail.com>
2024-03-08 22:37:18 +0600

    search bar added

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import SearchBar from "./components/SearchBar";
+
+function App() {
+  return (
+    
+    <div className="max-w-6xl mx-auto mt-10">
+       <div className="flex justify-between">
+
+      <div> <SearchBar></SearchBar> </div>
+      <div>Tabs</div>
+      <div>logind</div>
+    </div>
+   </div>
+  );
+}
+
+export default App;
